8d2cf26156b4b3081417d6997aa927e8920d5c11
Dooruss <doorusdebeijer@gmail.com>
2026-06-25 09:34:36 +0200

    Cube

---
+++ idle-game/Cube.gd
@@ -0,0 +1,64 @@
+extends Area2D
+
+# Area2D = GameObject with a trigger Collider2D
+# Signals bubble up to Main.gd same as UnityEvents
+
+signal cube_clicked(amount: int)
+signal passive_tick(amount: int)
+
+# @onready = GetComponent / Find in Start()
+@onready var passive_timer: Timer = $PassiveTimer
+@onready var click_label:   Label = $ClickLabel
+
+var click_value:   int = 1
+var passive_value: int = 1
+
+var _tween: Tween  # like a DOTween sequence reused each click
+
+
+func _ready() -> void:
+	passive_timer.wait_time = 1.0
+	passive_timer.autostart = true
+	passive_timer.start()
+	click_label.visible = false
+
+
+func on_clicked() -> void:
+	emit_signal("cube_clicked", click_value)
+	_wiggle()
+	_show_click_label(click_value)
+
+
+# Does every second
+func _on_passive_timer_timeout() -> void:
+	emit_signal("passive_tick", passive_value)
+
+# Lowkey forgot
+func _wiggle() -> void:
+	if _tween:
+		_tween.kill()
+	_tween = create_tween()
+	_tween.set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_OUT)
+	_tween.tween_property(self, "scale", Vector2(1.3, 0.75), 0.07)
+	_tween.tween_property(self, "scale", Vector2(1.0, 1.0),  0.30)
+
+
+func _show_click_label(amount: int) -> void:
+	click_label.text       = "+%d" % amount
+	click_label.modulate.a = 1.0
+	click_label.visible    = true
+
+	var origin_y := click_label.position.y
+	var pop := create_tween()
+	pop.tween_property(click_label, "position:y", origin_y - 36.0, 0.5)
+	pop.parallel().tween_property(click_label, "modulate:a", 0.0, 0.5)
+	await pop.finished
+
+	click_label.position.y = origin_y
+	click_label.visible    = false
+
+
+# Called by the main script after an upgrade purchase
+func apply_upgrades(new_click_val: int, new_passive_val: int) -> void:
+	click_value   = new_click_val
+	passive_value = new_passive_val
--- idle-game/main.gd
+++ idle-game/main.gd
@@ -1,61 +1,143 @@
 extends Node2D
 
-@onready var multi_button: Button = $UpperController/MultiButton
-@onready var auto_button: Button = $UpperController/AutoButton
-@onready var auto_amount_button: Button = $UpperController/AutoAmountButton
-@onready var CoinAmountLabel: RichTextLabel = $UpperController/CoinAmountLabel
-@onready var auto_t_ime: Timer = $UpperController/AutoButton/AutoTIme
+# preload = Resources.Load<GameObject>("path"), loads the Cube prefab
+const CUBE_SCENE := preload("res://Cube.tscn")
 
-var Coins:int = 0
-var AutoEnable:bool = false
-var AutoAmount:int = 1
-var ClickAmount:int = 1
-var MultiLevel:int = 0
+# @onready = GetComponent / Find called in Start()
+@onready var coin_label:             Label  = $UI/CoinLabel
+@onready var info_label:             Label  = $UI/InfoLabel
+@onready var earn_button:            Button = $UI/EarnButton
+@onready var buy_cube_button:        Button = $UI/BuyCubeButton
+@onready var click_upgrade_button:   Button = $UI/ClickUpgradeButton
+@onready var passive_upgrade_button: Button = $UI/PassiveUpgradeButton
 
-# Called when the node enters the scene tree for the first time.
+# CubeContainer is an empty Node2D used as a parent folder for spawned cubes
+# Same pattern as an empty GameObject used as a container in Unity
+@onready var cube_container: Node2D = $CubeContainer
+
+var coins: int       = 0
+var cubes_owned: int = 0
+
+var click_upgrade_level:   int = 0
+var passive_upgrade_level: int = 0
+
+# These are recalculated after every upgrade and pushed to all cubes
+var click_value:   int = 1
+var passive_value: int = 1
+
+var cube_cost:            int = 50
+var click_upgrade_cost:   int = 25
+var passive_upgrade_cost: int = 100
+
+# Grid layout for cube placement
+const CUBE_COLUMNS: int    = 6
+const CUBE_SPACING: int    = 80
+const GRID_ORIGIN: Vector2 = Vector2(140, 80)
+
+
+# _ready = Start()
 func _ready() -> void:
-	pass # Replace with function body.
+	_refresh_ui()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	CoinAmountLabel.text = str(Coins)
+# _process = Update() only used to keep the coin label updated
+func _process(_delta: float) -> void:
+	_refresh_ui()
 
 
-func _on_click_button_pressed() -> void:
-	AddCoins(ClickAmount)
+func _refresh_ui() -> void:
+	var cps := cubes_owned * passive_value
+	coin_label.text = "Coins: %d  (+%d/sec)" % [coins, cps]
+	info_label.text = "Cubes: %d" % cubes_owned
+
+	earn_button.text            = "Click  [+%d]" % click_value
+	buy_cube_button.text        = "Buy Cube  [$%d]" % cube_cost
+	click_upgrade_button.text   = "Click Upgrade Lv%d  [$%d]" % [click_upgrade_level + 1, click_upgrade_cost]
+	passive_upgrade_button.text = "Passive Upgrade Lv%d  [$%d]" % [passive_upgrade_level + 1, passive_upgrade_cost]
+
+	# Disable buttons the player cant buy
+	buy_cube_button.disabled        = coins < cube_cost
+	click_upgrade_button.disabled   = coins < click_upgrade_cost
+	passive_upgrade_button.disabled = coins < passive_upgrade_cost
+
+
+# Button callbacks (in main scene, same as onClick in Unity)
+
+func _on_earn_button_pressed() -> void:
+	coins += click_value
+
+
+func _on_buy_cube_button_pressed() -> void:
+	if coins < cube_cost:
+		return
+	coins -= cube_cost
+	cube_cost = int(cube_cost * 1.35)
+	_spawn_cube()
+
+
+func _on_click_upgrade_button_pressed() -> void:
+	if coins < click_upgrade_cost:
+		return
+	coins -= click_upgrade_cost
+	click_upgrade_level += 1
+	click_value = 1 + (click_upgrade_level * click_upgrade_level)
+	click_upgrade_cost = int(click_upgrade_cost * 2.0)
+	_apply_upgrades_to_all_cubes()
+
+
+func _on_passive_upgrade_button_pressed() -> void:
+	if coins < passive_upgrade_cost:
+		return
+	coins -= passive_upgrade_cost
+	passive_upgrade_level += 1
+	passive_value = 1 + passive_upgrade_level
+	passive_upgrade_cost = int(passive_upgrade_cost * 2.2)
+	_apply_upgrades_to_all_cubes()
+
+
+# Spawning the cubes
+
+func _spawn_cube() -> void:
+	# instantiate() = Instantiate(prefab) in Unity
+	var cube: Area2D = CUBE_SCENE.instantiate()
+
+	var col := cubes_owned % CUBE_COLUMNS
+	var row := cubes_owned / CUBE_COLUMNS
+	cube.position = GRID_ORIGIN + Vector2(col * CUBE_SPACING, row * CUBE_SPACING)
+
+	# add_child = transform.SetParent in Unity
+	cube_container.add_child(cube)
+	cube.apply_upgrades(click_value, passive_value)
+
+	# connect() = AddListener() in Unity
+	cube.cube_clicked.connect(_on_cube_clicked)
+	cube.passive_tick.connect(_on_cube_passive_tick)
+
 	
-	
-func AddCoins(amount):
-	Coins += amount
+	cube.input_pickable = true
+	cube.connect("input_event", _make_cube_click_handler(cube))
+
+	cubes_owned += 1
 
 
-func _on_auto_t_ime_timeout() -> void:
-	if AutoEnable == true:
-		AddCoins(AutoAmount)
+func _make_cube_click_handler(cube: Area2D) -> Callable:
+	return func(_viewport, event: InputEvent, _shape_idx):
+		if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			cube.on_clicked()
 
 
-func _on_auto_button_pressed() -> void:
-	if Coins >= 100 and AutoEnable != true:
-		Coins -= 100
-		AutoEnable = true
-		auto_t_ime.start(1)
+# Alot of coin function
 
-func _on_multi_button_pressed() -> void:
-	if Coins >= (25 * MultiLevel):
-		Coins -= (25 * MultiLevel)
-		MultiLevel += 1
-		ClickAmount = MultiLevel * MultiLevel
-		var FormatString = "More per Click!: $%s"
-		var ActualString = FormatString % str(25 * MultiLevel)
-		multi_button.text = ActualString
-		
+func _on_cube_clicked(amount: int) -> void:
+	coins += amount
 
 
-func _on_auto_amount_button_pressed() -> void:
-	if Coins >= 100:
-		AddCoins(-100)
-		AutoAmount += 2
-		var FormatString = "More per second!: $%s"
-		var ActualString = FormatString % str(100)
-		auto_amount_button.text = ActualString
+func _on_cube_passive_tick(amount: int) -> void:
+	coins += amount
+
+
+# updates values to every cube (like foreach on a List<Cube>)
+func _apply_upgrades_to_all_cubes() -> void:
+	for child in cube_container.get_children():
+		if child.has_method("apply_upgrades"):
+			child.apply_upgrades(click_value, passive_value)
